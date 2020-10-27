9d3cba3d9f0dd63b008ef4c37588a20a93fa15ab
Tim Herbert <tcherbert@lcmail.lcsc.edu>
2020-10-27 12:38:22 -0700

    Fixed folder-creation.py and cleaned up api-call.py

--- folder-creation.py
+++ folder-creation.py
@@ -27,19 +27,34 @@ while True:
         mydict[state][county].append(zipcode)
 
     
-#print(mydict)
+#print(mydict['WA'])
 
 #Work in progress...
 path = os.getcwd()
+
+for i in mydict.keys():
+    state_path = path + "/" + i
+    os.mkdir(state_path)
+    for j in mydict[i].keys():
+        county_path = state_path + "/" + j
+        os.mkdir(county_path)
+        for n in mydict[i][j]:
+            zip_path = county_path + "/" + n
+            os.mkdir(zip_path)
+
+
+
 for state in mydict:
-    print(state)
+    # print("\n\nHello New State")
+    # print(state)
     #state_path = path + "/" + state
     #print(state_path)
     #os.mkdir(state_path)
     #if isinstance(state,dict):
-    for county, value in state:
-        print(county)
-        print(value)
+    for county in state:
+        pass
+        # print(county)
+        # print(value)
         #county_path = state_path + "/" + county
         #print(county_path)
         #os.mkdir(county_path)
