5378c110a4dcbb53e11f64ce194b22d9a85383a8
Tim Herbert <tcherbert@lcmail.lcsc.edu>
2020-10-29 13:48:30 -0700

    Updated test-api-call.py

--- test-api-call.py
+++ test-api-call.py
@@ -2,46 +2,34 @@ import requests
 import time
 
 headers     = {'token': 'gHlnfzHkxbaAlsIwGrxtTPEYmwgVjXpv'}
-error_log   = open('error-log.txt','w')
-results     = open('results.txt','rw')
-fp          = open('ZIP-COUNTY-FIPS_2010-03.csv','r')
-codes_429   = open('error-429.txt','w')
-mindate     = 0
-
-
-#Just skip first line -- Header Line
-line = fp.readline()
-
-mydict = {}
-
-while True:
-    line = fp.readline()
-    if line == '':
-        break
-    row = line.split(",")
-
-    zipcode     = row[0]
-    county      = row[1]
-    state       = row[2]
-
-    if state not in mydict:
-        mydict[state] = {}
-
-    if not county in mydict[state]:
-        mydict[state].update({county:[]})
-    
-    if not zipcode in mydict[state][county]:
-        mydict[state][county].append(zipcode)
+results     = open('results.txt','a')
+counter     = 0
+limit       = 1000
 
 
 def process():
     print('inside process')
-    r = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=ZIP&sortfield=name&sortorder=desc&offset=25&limit=200', headers = headers)
+    r = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=ZIP&sortfield=name&sortorder=desc&offset=' + str(counter) + '&limit=' + str(limit), headers = headers)
     # print(r.status_code)
     #Success
     if r.status_code == 200:
-        response_dict = r.json()
-        response_text = r.text
         results.write(r.text + '\n')
+        
+        #can look at the other script to improve with other codes and such but this api is so simple you don't need it.
+        #Could also get mindate easily by iterateing over the results or get it all then find it that way.
+        #We then gotta make a decsion as to how we are going to manage this project in regard to start date. 
+        #Either by making sure there is one zipcode for that county earlier than that or just do like we stated earlier no data for that county as of that date.. 
+        
 
-process()
+
+
+while True:
+    if counter + 1000 < 30415:
+        process()
+        counter += 1000
+    else:
+        #Final run and done.
+        limit = 30415 - counter
+        counter = limit
+        process()
+        break
