8bb7175461ffed2d4b64875fc3eec1b48fea6139
Tim Herbert <tcherbert@lcmail.lcsc.edu>
2020-11-30 12:40:17 -0800

    final push

--- test-fcc.py
+++ test-fcc.py
@@ -17,12 +17,25 @@ for filename in os.listdir(path):
     print(line)
     break
 
+
+
+#pseudo code....
+#iterate over dataset
+
+#get state,county information from FCC Database. 
+    #break if 429 for limiting for ipaddress switch...
+
+#create folder structure for state / county
+    #store data by years.. making sure to append new data
+
+
+
+
 #"34.23333","-86.16667"
 def process():
 
     requestString = 'https://geo.fcc.gov/api/census/area?lat=34.23333&lon=-86.16667'
     
-
     r = requests.get(requestString)
     print(r.status_code)
     #Success
